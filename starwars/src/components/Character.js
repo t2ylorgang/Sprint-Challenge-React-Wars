7e4fbcc5fff71c8021b520870a023c3c54a07960
Taylor Olson <pipestay622@gmail.com>
2020-06-01 23:05:22 -0700

    Taylor-Olson

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const Character = (props) => {
+  return (
+    <div class = "d-flex justify-content-center align-items-center">
+      <Card style = {{ width: '400px' }}>
+        <CardImg top width="400px" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Character;
